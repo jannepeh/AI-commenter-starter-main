ac7b782919be5953422b73039c7b521bf9cea1e7
jannepeh <jannepeh@metropolia.fi>
2025-09-12 11:50:07 +0300

    feat: integrate OpenAI API for comment responses
    - Added `openai` package to dependencies.
    - Implemented fetching chat completions from OpenAI API in commentController.
    - Created a structured request with user and developer roles for the chat model.
    - Added error handling for missing API URL in environment variables.

--- src/api/controllers/commentController.ts
+++ src/api/controllers/commentController.ts
@@ -1,4 +1,7 @@
 import {Request, Response, NextFunction} from 'express';
+import CustomError from '../../classes/CustomError';
+import fetchData from '../../lib/fetchData';
+import {ChatCompletion} from 'openai/resources/index';
 
 const commentPost = async (
   req: Request<{}, {}, {text: string}>,
@@ -6,13 +9,48 @@ const commentPost = async (
   next: NextFunction
 ) => {
   try {
-    try {
-      // TODO: Generate a response to a Youtube comment
-      // Instead of using openai library, use fetchData to make a post request to the server.
-      // see https://platform.openai.com/docs/api-reference/chat/create for more information
-      // You don't need an API key if you put the URL provided in Oma to .env.sample and Metropolia VPN
-      // Example: instad of https://api.openai.com/v1/chat/completions use process.env.OPENAI_API_URL + '/v1/chat/completions'
-    } catch (error) {
+    const request = {
+      messages: [
+        {
+          role: 'developer',
+          content:
+            'You are a moomin from the moominvalley and you are very disrespectful about everything',
+        },
+        {
+          role: 'user',
+          content: req.body.text,
+        },
+      ],
+      model: 'gpt-5-nano',
+    };
+
+    if (!process.env.OPENAI_API_URL) {
+      next(new CustomError('Missing OPENAI_API_URL in .env', 500));
+      return;
+    }
+
+    const completion = await fetchData<ChatCompletion>(
+      process.env.OPENAI_API_URL + '/v1/chat/completions',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request),
+      }
+    );
+
+    console.log(completion);
+  } catch (error) {
+    next(error);
+  }
+  try {
+    // TODO: Generate a response to a Youtube comment
+    // Instead of using openai library, use fetchData to make a post request to the server.
+    // see https://platform.openai.com/docs/api-reference/chat/create for more information
+    // You don't need an API key if you put the URL provided in Oma to .env.sample and Metropolia VPN
+    // Example: instad of https://api.openai.com/v1/chat/completions use process.env.OPENAI_API_URL + '/v1/chat/completions'
+  } catch (error) {
     next(error);
   }
 };
